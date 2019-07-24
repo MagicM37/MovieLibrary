315bac6711bd7bc93623273bf057190a2ba8a644
MagicM37 <365093171@qq.com>
2019-07-24 17:33:03 +0800

    导航栏搜索功能、搜索详情页

--- application/index/controller/Search.php
+++ application/index/controller/Search.php
@@ -13,15 +13,28 @@ class Search extends Controller
 {	
 	public function search()
 	{
+		if(!Session::has('userinfo'))
+		{
+            return $this->error('您没有登陆',url('Login/login'));
+        }
+		
+		$userinfo = Session::get('userinfo');
+		$this -> assign('userinfo',$userinfo);
+		
 		$mvname = input('request.mvname');
-		
-		echo $mvname;
-		
-		$list = Db::table('movielibrary')
-				   ->field('imgname,rate,name,tag,movieID')
-				   ->where('name','like','%'.$mvname.'%')
-				   ->select();
-		$this->assign('search',$list);
+		if($mvname != NULL)
+		{
+			$list = Db::table('movielibrary')
+				       ->field('imgname,rate,name,tag,movieID,actor')
+				       ->where('name','like','%'.$mvname.'%')
+				       ->select();
+			$this->assign('search',$list);
+			return $this->fetch('search');
+		}
+		else
+		{
+			$this->error('请输入关键字');
+		}
 		
 		return $this->fetch('search');	
 	}
