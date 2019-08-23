a8156b25d6fc39bc9045e9f0600596d8ea204058
MagicM37 <365093171@qq.com>
2019-08-23 12:53:08 +0800

    修改readme

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'movie',
+    'database'        => '',
     // 用户名
-    'username'        => 'movie',
+    'username'        => '',
     // 密码
-    'password'        => 'movie160080',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
